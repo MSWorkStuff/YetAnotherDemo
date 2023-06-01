3981c57480ce4f46c9ed002e4f33a11ca5e67019
José Álvarez <josealvar@microsoft.com>
2023-06-01 15:21:32 -0700

    WIP

--- app/src/main/java/openai/azure/ui/views/MainActivity.kt
+++ app/src/main/java/openai/azure/ui/views/MainActivity.kt
@@ -1,25 +1,65 @@
+@file:OptIn(ExperimentalMaterial3Api::class)
+
 package openai.azure.ui.views
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import kotlinx.coroutines.flow.onEach
+import openai.azure.R
+import openai.azure.ui.services.OpenAIService
 import openai.azure.ui.theme.JavaDemoTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        val openAIService = OpenAIService()
+        val viewModel = MainViewModel(openAIService)
+
         super.onCreate(savedInstanceState)
         setContent {
             JavaDemoTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background) {
+                    CenterColumn(viewModel)
+                }
+            }
+        }
+        bindUIState(viewModel)
+    }
+
+    private fun bindUIState(viewModel: MainViewModel) {
+        viewModel.uiState.onEach {uiState: UIState ->
+            when(uiState) {
+                is UIState.Loading -> {
+                    // TODO reflect in UI
+                }
+                is UIState.CompletionsReady -> {
+                    val completions = uiState.completions
+                    // TODO: reflect in UI
                 }
             }
         }
@@ -27,17 +67,35 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-            text = "Hello $name!",
-            modifier = modifier
-    )
+fun CenterColumn(viewModel: MainViewModel) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+    ) {
+        var text by remember { mutableStateOf(TextFieldValue("")) }
+        
+        TextField(
+            value = text,
+            onValueChange = {
+                text = it
+            },
+            label = { Text(text = "Prompt") },
+            placeholder = { Text(text = stringResource(id = R.string.description_label)) },
+        )
+        
+        Button(onClick = {
+                viewModel.getCompletions(text.text)
+        }) {
+            Text(text = "Submit")
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     JavaDemoTheme {
-        Greeting("Android")
+        CenterColumn()
     }
 }
--- app/src/main/java/openai/azure/ui/views/MainViewModel.kt
+++ app/src/main/java/openai/azure/ui/views/MainViewModel.kt
@@ -1,25 +1,31 @@
 package openai.azure.ui.views
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.azure.ai.openai.models.Completions
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
 import openai.azure.ui.services.OpenAIService
 
 class MainViewModel(private val openAIService: OpenAIService): ViewModel() {
 
-    private val _uiState = MutableStateFlow(UIState.Loading)
+    private val _uiState = MutableStateFlow<UIState>(UIState.Loading)
     val uiState: StateFlow<UIState> = _uiState.asStateFlow()
 
-    suspend fun getCompletions(prompt: String) {
-        openAIService.getCompletions(prompt)
+    fun getCompletions(prompt: String) {
+        viewModelScope.launch {
+            _uiState.emit(UIState.Loading)
+            val completions = openAIService.getCompletions(prompt)
+            _uiState.emit(UIState.CompletionsReady(completions))
+        }
     }
 
 }
 
 sealed class UIState {
     object Loading: UIState()
-    class CompletionsReady(val completions: Completions)
+    class CompletionsReady(val completions: Completions): UIState()
 
 }
